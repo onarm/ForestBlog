60d0205006237ad9f15c7f6bf526c1484238b058
onarm <2470660073@qq.com>
2023-09-25 01:34:37 +0800

    一次性提交

---
+++ src/main/java/com/dubuyin/ssm/blog/service/impl/OptionsServiceImpl.java
@@ -0,0 +1,40 @@
+package com.dubuyin.ssm.blog.service.impl;
+
+import com.dubuyin.ssm.blog.entity.Options;
+import com.dubuyin.ssm.blog.mapper.OptionsMapper;
+import com.dubuyin.ssm.blog.service.OptionsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author 独步吟
+ * @date 2017/9/7
+ */
+@Service
+public class OptionsServiceImpl implements OptionsService {
+
+
+    @Autowired(required = false)
+    private OptionsMapper optionsMapper;
+
+    @Override
+    @Cacheable(value = "default", key = "'options'")
+    public Options getOptions() {
+        return optionsMapper.getOptions();
+    }
+
+    @Override
+    @CacheEvict(value = "default", key = "'options'")
+    public void insertOptions(Options options) {
+        optionsMapper.insert(options);
+    }
+
+    @Override
+    @CacheEvict(value = "default", key = "'options'")
+    public void updateOptions(Options options) {
+        optionsMapper.update(options);
+    }
+}
